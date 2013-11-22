5b2f6dfd0de9c554474047fb8d81cc15ff2f2f82
cpeter9 <cpeter9@gmail.com>
2013-11-22 12:40:51 -0500

    I created the first histogram

--- introduction.R
+++ introduction.R
@@ -1,3 +1,11 @@
 # This is the introduction to ggplot2
 # Author: Christopher Peters
 # Email: statwonk@gmail.com
+
+# install.packages("ggplot2")
+library(ggplot2)
+
+str(iris)
+
+ggplot(data = iris, aes(x = Sepal.Length)) +
+  geom_histogram()
