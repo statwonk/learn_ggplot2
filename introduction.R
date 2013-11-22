c119619b7481ef1d8ffb901f272627caa0649fd6
cpeter9 <cpeter9@gmail.com>
2013-11-22 13:00:25 -0500

    Showing how themes and changing axis text and color works

--- introduction.R
+++ introduction.R
@@ -10,8 +10,25 @@ str(iris)
 ggplot(data = iris, aes(x = Sepal.Length)) +
   geom_histogram() # this is a simple example of a histogram
 
-ggplot(data = iris, aes(x = Sepal.Length)) +
+# install.packages("ggthemes")
+library(ggthemes)
+
+ggplot(data = iris, aes(x = Sepal.Length)) + 
   geom_histogram() + # this is a simple example of a histogram
   facet_wrap( ~ Species) +
-  ggtitle("Iris Dataset, learning ggplot!")
+  ggtitle("Iris Dataset, learning ggplot!") +
+  ylab("Frequency") +
+  xlab("Sepal Length (units = cm)") +
+  chris_theme()
+  
+chris_theme <- function(){ # this will likely be common across your 
+  # graphs in a single article, so why not drop it in a function and
+  # make it nice and simple to call?
+  theme_wsj() +
+  theme(axis.text = element_text(size = 16),
+        axis.text.x = element_text(colour = "red"),
+        strip.text = element_text(size = 20, colour = "blue"))
+}
+
+
 
